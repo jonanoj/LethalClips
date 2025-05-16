b9429d093dde3ed5745da66e7daae8416cb71701
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-05-16 12:41:17 +0300

    add events for round start and end

---
+++ LethalClips/Patches/StartOfRoundPatch.cs
@@ -0,0 +1,57 @@
+using System;
+using HarmonyLib;
+using LethalClips;
+using Steamworks;
+
+[HarmonyPatch(typeof(StartOfRound))]
+internal class StartOfRoundPatch
+{
+    [HarmonyPatch(nameof(StartOfRound.openingDoorsSequence))]
+    [HarmonyPrefix]
+    private static void OpeningDoorsSequence()
+    {
+        Plugin.Log.LogDebug("OpeningDoorsSequence called");
+
+        // TODO: consider using the SetTimelineGameMode API instead
+        try
+        {
+            var timelineEvent = SteamTimeline.AddInstantaneousTimelineEvent(
+                "Ship has landed",
+                "Round start",
+                "steam_flag",
+                0,
+                0,
+                TimelineEventClipPriority.Standard
+            );
+            Plugin.Log.LogDebug($"Added timeline event {timelineEvent}.");
+        }
+        catch (Exception e)
+        {
+            Plugin.Log.LogError($"Failed to add timeline event: {e}");
+        }
+    }
+
+    [HarmonyPatch(nameof(StartOfRound.ShipHasLeft))]
+    [HarmonyPrefix]
+    private static void ShipHasLeft()
+    {
+        Plugin.Log.LogDebug("ShipHasLeft called");
+
+        try
+        {
+            var timelineEvent = SteamTimeline.AddInstantaneousTimelineEvent(
+                "Ship has leff",
+                "Round ended",
+                "steam_completed",
+                0,
+                0,
+                TimelineEventClipPriority.Standard
+            );
+            Plugin.Log.LogDebug($"Added timeline event {timelineEvent}.");
+        }
+        catch (Exception e)
+        {
+            Plugin.Log.LogError($"Failed to add timeline event: {e}");
+        }
+    }
+}
